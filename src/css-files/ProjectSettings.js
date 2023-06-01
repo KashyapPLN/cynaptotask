5e72923ca31b9d6854f782fa825a42891a39eeaf
KashyapPLN <kashyap.pln@gmail.com>
2023-06-01 17:00:26 +0530

    added dropdown

--- src/css-files/ProjectSettings.js
+++ src/css-files/ProjectSettings.js
@@ -4,13 +4,17 @@ import aspectRatio from '../aspectRatioOptions.json';
 export default function ProjectSettings() {
   console.log(aspectRatio[0].name);
     const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(aspectRatio[0].name);
+  const [selectedName, setSelectedName] = useState(aspectRatio[0].name);
+  const [selectedIcon, setSelectedIcon] = useState(aspectRatio[0].icon);
+  const [selectedRatio, setSelectedRatio] = useState(aspectRatio[0].aspectRatio);
   const handleToggle = () => {
     setIsOpen(!isOpen);
   };
 aspectRatio.map((drop1)=>drop1)
-  const handleOptionSelect = (option) => {
-    setSelectedOption(option);
+  const handleOptionSelect = (name,icon,ratio) => {
+    setSelectedName(name);
+    setSelectedIcon(icon);
+    setSelectedRatio(ratio);
     setIsOpen(false);
   };
     return (
@@ -29,18 +33,20 @@ aspectRatio.map((drop1)=>drop1)
 
 <div className="custom-dropdown">
       <div className="dropdown-toggle " onClick={handleToggle}>
-      {selectedOption}
-        <img src="path_to_image" alt="Option" /> 
+      <img style={{width:'20px',height:'18px',marginRight:'10px'}} src={selectedIcon} alt="Option" /> 
+      {selectedName}
+      <span style={{color:'#8d8e95ab',marginLeft:'8px'}}>({selectedRatio})</span>
+        
         {/* Replace "path_to_image" with the actual path to your image */}
         
         {/* <span className={`arrow ${isOpen ? 'open' : ''}`}>&#9662;</span> */}
       </div>
-     {aspectRatio.map((drop)=> <div className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
-        <div className="dropdown-item" onClick={() => handleOptionSelect(drop.name)}>
-        <img style={{width:'20px',height:'18px',marginRight:'10px'}}src={drop.icon}/> {drop.name} <span style={{color:'#8d8e95ab',marginLeft:'8px'}}>({drop.aspectRatio})</span>
-        </div>
+    <div className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
+    {aspectRatio.map((drop)=> <div className="dropdown-item" onClick={() => handleOptionSelect(drop.name,drop.icon,drop.aspectRatio)}>
+       <img style={{width:'20px',height:'18px',marginRight:'10px'}}src={drop.icon}/> {drop.name} <span style={{color:'#8d8e95ab',marginLeft:'8px'}}>({drop.aspectRatio})</span>
+        </div>)}
         
-      </div>)}
+      </div>
     </div>
 
         </div>
